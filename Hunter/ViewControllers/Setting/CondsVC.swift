4de4864a112f07629a9387c2531f485cb4d99411
iOSayedAhmed <60572628+iOSayedAhmed@users.noreply.github.com>
2023-09-25 22:55:19 +0300

    Edit Settings

--- Hunter/ViewControllers/Setting/CondsVC.swift
+++ Hunter/ViewControllers/Setting/CondsVC.swift
@@ -20,8 +20,8 @@ class CondsVC: ViewController {
     
     let condsTextAR = """
     الشروط والأحكام :
-    مرحبا بكم في ( تطبيق مناسبة ) فضلا قراءة الشروط والأحكام بعناية قبل استخدام الموقع الإلكتروني أو التطبيق، تصفحك أو إضافة أي إعلان لدينا يعتبر إقرار منك بقراءة الشروط والأحكام والموافقة عليها.
-    تمتلك شركة اشحن للتجارة العامة  تطبيق و موقع ( مناسبة ) و هي منصة توفر مساحة للإعلانات لمستخدميها (البائعين والمشترين)، ويقتصر دور ( تطبيق مناسبة ) على توفير مساحة لالتقاء البائع والمشتري بصورة ميسرة، ولا تتحمل ( تطبيق مناسبة ) أي مسئولية ولا توفر أي ضمان اتجاه المستخدمين بما يتعلق بعمليات البيع والشراء وجودة المنتجات، ولا يتم الرجوع عليها للمطالبة بالتعويض عن أي خسائر.
+    مرحبا بكم في ( تطبيق الصياد ) فضلا قراءة الشروط والأحكام بعناية قبل استخدام الموقع الإلكتروني أو التطبيق، تصفحك أو إضافة أي إعلان لدينا يعتبر إقرار منك بقراءة الشروط والأحكام والموافقة عليها.
+    تمتلك شركة اشحن للتجارة العامة  تطبيق و موقع ( الصياد ) و هي منصة توفر مساحة للإعلانات لمستخدميها (البائعين والمشترين)، ويقتصر دور ( تطبيق الصياد ) على توفير مساحة لالتقاء البائع والمشتري بصورة ميسرة، ولا تتحمل ( تطبيق الصياد ) أي مسئولية ولا توفر أي ضمان اتجاه المستخدمين بما يتعلق بعمليات البيع والشراء وجودة المنتجات، ولا يتم الرجوع عليها للمطالبة بالتعويض عن أي خسائر.
 
     تعليمات وقواعد نشر الإعلان:
     يتم إدراج الإعلانات بعد التسجيل برقم الهاتف المحمول والبريد الإلكتروني.
@@ -41,14 +41,38 @@ class CondsVC: ViewController {
     يمنع نشر أي إعلان للخمور أو المشروبات الروحية بكافة أنواعها - كما تحظر نشر أي اي اعلان للمخدرات والحشيش بكافة أنواعها.
     يحظر عرض وبيع أجهزة التنصت و وسائل التحريض على إساءة استعمال أجهزة الهاتف، وذلك بحسب القانون رقم 9 لسنة 2001 بشأن إساءة استعمال أجهزة الاتصالات الهاتفية وأجهزة التنصت.
     يحظر الإعلان عن سداد القروض والتكييش، وكذلك يحظر الإعلان عن الشقق المفروشة بالساعات أو الإيجار اليومي لغير الشركات المرخص لها بذلك، وتحظر جميع إعلانات جلب الاستثمار والدعوة للاستثمار والمشاركة التجارية ،
-    تحتفظ إدارة ( تطبيق مناسبة) بحق حظر أي رقم هاتف يستخدم التطبيق في الإساءة أو الأضرار بالغير أو بحال استخدام التطبيق لأغراض غير قانونية.
+    تحتفظ إدارة ( تطبيق الصياد) بحق حظر أي رقم هاتف يستخدم التطبيق في الإساءة أو الأضرار بالغير أو بحال استخدام التطبيق لأغراض غير قانونية.
     يراعي المستخدم احترام المتصفحين والمستخدمين الآخرين.
     يمكن للمستخدمين التواصل فيما بينهم من خلال المحادثات ( شات ) والرسائل الصوتية ويمكنهم أيضا التعليقات علي الإعلانات والرد علي التعليق
     ويمكن للمستخدمين الإبلاغ علي المحتوي الغير مناسب او الأشخاص الغير مناسبين بالإضافة الي إمكانية عمل حظر للمستخدمين الغير مناسبين وعدم رؤيتهم او رؤية ما اعلاناتهم او تعليقاتهم
     """
     let condsTextEn = """
-    Terms and Conditions:
-    Welcome to (Musaba’a application). Please read the terms and conditions carefully before using the website or application. Your browsing or adding any advertisement we have is an acknowledgment of your reading and agreement to th
+    Terms and Conditions :
+    Welcome to (Hunter Application). Please read the terms and conditions carefully before using the website or application. Your browsing or adding any of our advertisements is considered your acknowledgment of reading the terms and conditions and agreeing to them.
+    Ashhan General Trading Company owns the () application and website, which is a platform that provides space for advertisements for its users (buyers and sellers). The role of (Hunter Application) is limited to providing a space for the seller and buyer to meet in an easy manner. (Hunter Application) does not bear any responsibility and does not provide any guarantee. The direction of users regarding buying and selling operations and the quality of products, and they are not subject to recourse to claim compensation for any losses.
+
+    Instructions and rules for publishing the advertisement:
+    Advertisements are inserted after registration with mobile number and email.
+    The advertiser must classify the ad and publish it under the correct section.
+    The title of the advertisement, product description, price, and pictures must be written correctly, and the item must be real and present.
+    The advertiser must be the owner of the good or service.
+    Advertisements must be in a personal capacity. It is prohibited to publish advertisements in a commercial capacity or on behalf of others. It is prohibited to publish advertisements for companies or stores.
+    It is prohibited to publish advertisements that contain words or images that violate public morals or are illegal. It is also prohibited to publish any advertisement that aims to defame people or institutions.
+    It is prohibited to publish advertisements for the sale of weapons of all types, whether licensed or unlicensed.
+    Each advertisement is for one commodity only, and in the event of multiple commodities, a separate advertisement is created for each commodity.
+    It is prohibited to publish advertisements for lawyers that aim to advertise the lawyer himself or work to attract clients.
+    It is prohibited to publish advertisements that violate the laws in the country of the advertisement’s creator. It is also prohibited to publish advertisements that violate the laws in the State of Kuwait, especially Law No. 63 of 2015 regarding combating electronic crimes, and Law No. 38 of 2002 regarding regulating the advertisement of health-related materials. It is prohibited to publish advertisements for medicines and sexual stimulants.
+    It is prohibited to publish smoking advertisements of all kinds, whether physical or electronic
+    It is prohibited to sell endangered plants and animals mentioned in the CITES agreement, in compliance with the decisions and regulations of the Environment Public Authority.
+    It is prohibited to publish advertisements for domestic workers of all kinds. It is also prohibited to publish advertisements for fake labor or for renting workers to homes during hours. It is also prohibited to publish all types of human trafficking.
+    It is prohibited to publish any advertisements for gambling, betting, or advertisements for magic and sorcery
+    It is prohibited to publish any advertisement for wines or spirits of all kinds - and it is also prohibited to publish any advertisement for drugs and hashish of all kinds.
+    It is prohibited to offer and sell eavesdropping devices and means of inciting the misuse of telephone devices, in accordance with Law No. 9 of 2001 regarding the misuse of telephone communications devices and eavesdropping devices.
+    It is prohibited to advertise the repayment of loans and air conditioning, and it is also prohibited to advertise furnished apartments for hours or daily rent to anyone other than companies licensed to do so, and all advertisements attracting investment and calling for investment and commercial participation are prohibited.
+    (Hunter Application) management reserves the right to block any phone number that uses the application to abuse or harm others or if the application is used for illegal purposes.
+    The user is respectful of other browsers and users.
+    Users can communicate with each other through chat and voice messages, and they can also comment on advertisements and respond to comments
+    Users can report inappropriate content or inappropriate people, in addition to the possibility of blocking inappropriate users and not seeing them or seeing their ads or comments.
     """
     override func viewDidLoad() {
         super.viewDidLoad()
